015fdfc361699fd107118422d9c5887593a03781
LOLbeans <luanmartinslopes25@gmail.com>
2023-11-09 15:05:21 -0300

    balinha ta pro

--- Assets/script/Bullet.cs
+++ Assets/script/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 2;
+    public float penetration = 0.2f;
 
     Rigidbody2D rb;
     // Start is called before the first frame update
@@ -17,5 +18,27 @@ public class Bullet : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector3(0, speed, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Map"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(penetration);
+        Destroy(gameObject);
     }
 }
