e53555b52f37ccf486c1704a0ef405fce7581fc4
LOLbeans <luanmartinslopes25@gmail.com>
2023-12-06 00:37:00 -0300

    Jump
    o pulo não está funcional ainda mas tem várias coisas prontas para o pulo no script
    Hitbox dos móveis estão completas

--- Assets/Script/Master.cs
+++ Assets/Script/Master.cs
@@ -13,6 +13,6 @@ public class Master : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(1 / Time.deltaTime);
+
     }
 }
--- Assets/Script/Players.cs
+++ Assets/Script/Players.cs
@@ -8,12 +8,15 @@ public class Players : MonoBehaviour
     public float moveSpeed = 2.0f;
     public float spinSpeed = 168.0f;
     public float spinResistence = 32.0f;
+    public Vector2 jumpSpeed;
 
     public string Horizontal;
     public string Vertical;
     public string Spin;
 
     public bool isGrounded = true;
+    public bool haveGround = true;
+    public bool isJumping = false;
     public Vector3 playerSize;
 
     // Start is called before the first frame update
@@ -27,34 +30,65 @@ public class Players : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
       // Move
         float moveHorizontal = Input.GetAxis(Horizontal);
         float moveVertical = Input.GetAxis(Vertical);
 
-        // Calcula o vetor de movimento
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        // Normaliza o vetor de movimento para que o jogador nao se mova mais rapido na diagonal
         movement = movement.normalized;
-        // Move o jogador usando o Rigidbody2D
-        rb.AddForce(movement * moveSpeed);
+        if (isGrounded)
+        {
+            rb.AddForce(movement * moveSpeed);
+            if (movement.magnitude > 0)
+            {
+                jumpSpeed = movement * moveSpeed;
+            }
+        }
+        else
+        {
+            rb.AddForce(jumpSpeed);
+        }
 
-      // Spin
+
+        // Spin
         if (movement != Vector2.zero)
         {
             Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movement);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, spinSpeed * Time.deltaTime);
         }
-
-
-        // Jump
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Floor"))
         {
-
+            haveGround = true;
+            Debug.Log("encostou em um móovel");
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            haveGround = false;
+            if (!isJumping)
+            {
+                StartCoroutine(JumpUp());
+            }
+            Debug.Log("saiu");
+        }
+           
+    }
+
+    IEnumerator JumpUp()
+    {
+        yield return new WaitForSeconds(0.16f);
+    }
+
+    IEnumerator JumpDown()
+    {
+        yield return new WaitForSeconds(0.16f);
     }
 }
